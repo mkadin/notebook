18fdc1d4ce04f8886ca21baeed94bfbd6f029711
mkadin <michaelkadin@gmail.com>
2012-10-02 14:19:00 -0400

    Adjusted upgrade.php to deal with new field

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 5;               // If version == 0 then module will not be installed
+$module->version   = 2012100200;               // If version == 0 then module will not be installed
 $module->requires  = 2010031900;      // Requires this Moodle version
 $module->cron      = 0;               // Period for cron to check this module (secs)
 $module->component = 'mod_notebook'; // To check on upgrade, that module sits in correct place
